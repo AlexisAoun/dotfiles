98ae4878f9625f62dcc3bbbf1037029c40fdb054
Alexis Aoun <pizzaman@localhost.localdomain>
2021-06-21 20:08:22 +0200

    beginning of new nvim lua config

---
+++ nvim/.config/nvim/lua/settings.lua
@@ -0,0 +1,21 @@
+-- Nvim Settings
+
+local o = vim.o
+local wo = vim.wo
+local bo = vim.bo
+
+
+o.number = true
+o.mouse = 'a'
+o.clipboard = 'unnamedplus'
+vim.cmd('set nohlsearch')
+vim.cmd('set nowrap')
+o.ignorecase = true
+o.smartcase = true
+o.incsearch = true
+o.relativenumber = true
+o.scrolloff = 10
+
+o.syntax = 'on'
+
+
