dfc48d1a8b9b398e34b9767432a6ee828175831b
Alexis Aoun <alexis.aoun@hotmail.com>
2022-02-08 16:54:02 +0100

    feat: new nvim minimal config

---
+++ nvim/.config/nvim/init.lua
@@ -0,0 +1,10 @@
+require "user.options"
+require "user.keymaps"
+require "user.plugins"
+require "user.cmp"
+require "user.lsp"
+require "user.treesitter"
+require "user.autopairs"
+
+vim.cmd("colorscheme gruvbox-material")
+
